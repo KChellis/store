c94613f0faaf0838a955c2315bcd477818b59fcf
Kristen Chellis <peantuster@gmail.com>
2018-05-08 09:36:24 -0700

    add edit and delete functionality

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -5,17 +5,33 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Injectable()
 export class AlbumService {
   albums: FirebaseListObservable<any[]>;
-  constructor(private database: AngularFireDatabase){ this.albums = database.list('albums');
-}
+  constructor(private database: AngularFireDatabase){
+    this.albums = database.list('albums');
+  }
 
 
   getAlbums() {
     return this.albums;
   }
+
   addAlbum(newAlbum: Album) {
   this.albums.push(newAlbum);
-}
+  }
+
   getAlbumById(albumId: string){
   return this.database.object('albums/' + albumId);
  }
+
+ updateAlbum(localUpdatedAlbum){
+   let albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
+   albumEntryInFirebase.update({title: localUpdatedAlbum.title,
+                               artist: localUpdatedAlbum.artist,
+                               description: localUpdatedAlbum.description});
+ }
+
+ deleteAlbum(localAlbumToDelete){
+    let albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
+
 }
